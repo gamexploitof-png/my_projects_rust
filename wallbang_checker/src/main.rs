0a4145717779327bd896aea02b526e95f8fa41e3
gamexploitof-png <gamexploitof@gmail.com>
2026-03-15 20:06:20 +0200

    modified:   wallbang_checker/src/main.rs

--- wallbang_checker/src/main.rs
+++ wallbang_checker/src/main.rs
@@ -12,6 +12,15 @@ struct Bullet {
     caliber: i32,
 }
 
+fn bullet_count(damage: &mut f64, max_hp: &mut f64) -> i32 {
+    let mut count:i32 = 0;
+    while (*damage * count as f64) < *max_hp {
+        count+=1;
+    }
+    
+    return count
+}
+
 fn main() {
     let mut material:Material;
     let mut input:String = String::new();
@@ -20,6 +29,7 @@ fn main() {
         damage: 34.0, // Урон в тело в CS2
         caliber: 762,
     };
+    let mut max_hp: f64 = 100.0;
     loop{
         let mut input = String::new();
         println!("Выберите материал:");
@@ -38,8 +48,8 @@ fn main() {
             Material::Steel => damage = ak47.damage * 0.2,
             Material::Concrete => damage = ak47.damage * 0.75, 
         }
-        println!("Damage: {}", damage)
-
+        println!("Damage: {}", damage);
+        println!("Times you need to shoot with such wallbangs to kill someone with {max_hp}hp: {}", bullet_count(&mut damage, &mut max_hp))
     }
     
 }
